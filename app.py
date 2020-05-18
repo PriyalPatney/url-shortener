ab992313ef33ebb64a2246778ca6b29d0348b2a0
Priyal Patney <priyal_patney@hotmail.com>
2020-05-19 03:25:56 +0530

    post req p2

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 # this is nothing but the name of our flask app
 app = Flask(__name__)
 
 
 @app.route("/")
-def about():
-    return 'hello there this is the'  # python will send this http res
-
-
-@app.route("/home")
 def home():
-    return render_template('home.html', name="Priyal")  # pssing templates with jinja
+    # pssing templates with jinja
+    return render_template('home.html', name="Priyal")
 
 
-@app.route("/your_url")
+@app.route("/your_url", methods=['GET', 'POST'])
 def your_url():
-    return render_template('your_url.html', code=request.args['code']) #using request info is passed through post req by form
+    if request.method == 'POST':
+        # using request info is passed through post req by for
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('home')) # or redirect('/home')
